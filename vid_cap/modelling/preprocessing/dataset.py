89543f52e79fdf120bb151c7481e7ae140565eb6
Joan Pascual <joanpascualgrau@gmail.com>
2023-06-19 13:06:27 +0200

    + Add _vocab property in dataset class. + Add function to build vocabulary in dataset class

--- vid_cap/modelling/preprocessing/dataset.py
+++ vid_cap/modelling/preprocessing/dataset.py
@@ -2,7 +2,7 @@
 """Dataset class for loading video feature vector data."""
 import random
 from pathlib import Path
-
+import collections
 import joblib
 import numpy as np
 from torch.utils.data import Dataset
@@ -13,41 +13,71 @@ class VideoFeatDataset(Dataset):
     """Dataset with video feature vectors and associated captions."""
 
     _data: list[tuple[np.ndarray, str]]
+    _vocab: list[str]
 
     def __init__(
-        self, data_dict: dict[str, dict[str, int | np.ndarray | list[str]]], captions_amount_per_video: int = 1
+        self, data_dict: dict[str, dict[str, int | np.ndarray | list[str]]], captions_amount_per_video: int = 1, vocab_len: int = 10000
     ) -> None:
         """Initialize dataset with data dictionary.
 
         :param data_dict: Dictionary with data.
         :param captions_amount_per_video: Amount of captions per video, min=1, max=20. Defaults to 1.
+        :param vocab_len: Amount of words for vocabulary. Defaults to 10000.
         """
         captions_amount_per_video = min(max(captions_amount_per_video, 1), 20)
 
+        self._data = self._sort_data_by_caption_lenght(data_dict, captions_amount_per_video)
+        self._vocab = self._build_vocab(vocab_len)
+
+    def _sort_data_by_caption_lenght(self, data_dict: dict[str, dict[str, int | np.ndarray | list[str]]], captions_amount_per_video: int) -> list[tuple[np.ndarray, str]]:
+        """Build vocabulary from the captions in the dataset.
+
+        :param data_dict: Dictionary with data.
+        :param captions_amount_per_video: Amount of captions per video.
+        :return: Truncated sorted by frecuency vocab.
+        """
         data = [
             (data["features"], caption)
             for data in data_dict.values()
             for caption in data["captions"][:captions_amount_per_video]
         ]
 
-        # Sort the data by caption length
         sorted_data = sorted(data, key=lambda x: len(x[1]))
+        return sorted_data
     
-        self._data = sorted_data
+    def _build_vocab(self, vocab_len: int) -> list[str]:
+        """Build vocabulary from the captions in the dataset.
+
+        :param vocab_len: Amount of words for vocabulary.
+        :return: Truncated sorted by frecuency vocab.
+        """
+        captions = [caption for _, caption in self._data]
+        words = [word for caption in captions for word in caption.split()]
+        vocab = collections.Counter(words)
+        sorted_vocab = sorted(vocab, key=lambda x: vocab[x], reverse=True)
+
+        sorted_vocab.insert(0, "<eos>")
+        sorted_vocab.insert(0, "<sos>")
+        sorted_vocab.insert(0, "<unk>")
+        sorted_vocab.insert(0, "<pad>")
+
+        truncated_sorted_vocab = sorted_vocab[:vocab_len]
+        return truncated_sorted_vocab
 
     @classmethod
-    def load(cls, file_path: Path | str, captions_amount_per_video: int = 1) -> Self:
+    def load(cls, file_path: Path | str, captions_amount_per_video: int = 1, vocab_len: int = 1000) -> Self:
         """Load dataset from file.
 
         :param file_path: Path to file with data.
         :param captions_amount_per_video: Amount of captions per video, min=1, max=20. Defaults to 1.
+        :param vocab_len: Amount of words for vocabulary. Defaults to 10000.
         :return: Dataset with data from file.
         """
         captions_amount_per_video = min(max(captions_amount_per_video, 1), 20)
 
         data_dict = joblib.load(file_path)
         
-        return cls(data_dict, captions_amount_per_video)
+        return cls(data_dict, captions_amount_per_video, vocab_len)
 
     def __getitem__(self, index: int) -> tuple[np.ndarray, str]:
         """Get item from dataset.
