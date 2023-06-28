80c2f5558ca36ac879d44f770f978b55ca433c3e
Daniel Martín <danitiana98@gmail.com>
2023-06-28 17:11:58 +0200

    Dataset initialization

--- vid_cap/__main__.py
+++ vid_cap/__main__.py
@@ -1,55 +1,58 @@
 # -*- coding: utf-8 -*-
 # ruff: noqa: D401
 """Entry point."""
-# from . import __version__
-from vid_cap.model.train import DecoderTrainer
-from vid_cap.model.__model_transformer_decoder import TransformerNet
-from vid_cap.modelling.preprocessing import VideoFeatDataset 
-from torch.utils.tensorboard import SummaryWriter
-from torch.utils.data import DataLoader
-
-import torch.optim as optim
 import torch
-import torch.nn as nn
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
+from vid_cap import DATA_DIR
+from vid_cap.model.__model_transformer_decoder import TransformerNet
+from vid_cap.model.train import DecoderTrainer
+from vid_cap.modelling.preprocessing import VideoFeatDataset
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
+
 def _main() -> None:
     """Run main function for entrypoint."""
-
     # @click.group(chain=True)
     # @click.version_option(__version__)
     # def entry_point() -> None:
     """Package entry point."""
 
-    # entry_point.add_command(dummy.main)
-
-    # entry_point()
-
-    train_dataset = VideoFeatDataset.load("../data/Dataset/Train/train.pickle") # Change path
-    valid_dataset = VideoFeatDataset.load("C:/Users/joanh/repos/video-caption/data/Dataset/Valid/val.pickle")# Change path
-    test_dataset = VideoFeatDataset.load("../data/Dataset/Train/train.pickle") # Change path
+    train_dataset = VideoFeatDataset(
+        DATA_DIR / "train" / "videos", DATA_DIR / "train" / "captions.parquet"
+    )
+    valid_dataset = VideoFeatDataset(
+        DATA_DIR / "val" / "videos", DATA_DIR / "train" / "captions.parquet"
+    )
+    test_dataset = VideoFeatDataset(
+        DATA_DIR / "test" / "videos", DATA_DIR / "train" / "captions.parquet"
+    )
 
     train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
     valid_loader = DataLoader(valid_dataset, batch_size=config["batch_size"], shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=config["batch_size"], shuffle=True)
 
     my_model = TransformerNet(
-        config["num_tgt_vocab"], 
+        config["num_tgt_vocab"],
         config["embedding_dim"],
-        config["vocab_size"], 
-        config["nheads"], 
+        config["vocab_size"],
+        config["nheads"],
         config["n_layers"],
-        config["max_tgt_len"]
+        config["max_tgt_len"],
     ).to(device)
 
     optimizer = optim.Adam(my_model.parameters(), config["lr"])
     criterion = nn.CrossEntropyLoss()
     writer = SummaryWriter()
 
-    dec_trainer = DecoderTrainer(optimizer, criterion, train_loader, valid_loader, my_model, train_dataset._vocab)
+    dec_trainer = DecoderTrainer(
+        optimizer, criterion, train_loader, valid_loader, my_model, train_dataset._vocab
+    )
     dec_trainer.train(config["epochs"], writer)
+
 
 if __name__ == "__main__":
     config = {
