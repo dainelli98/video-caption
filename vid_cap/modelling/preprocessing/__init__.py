49ac89a43341ff63595d85a519fec6fe34a42513
Daniel Martín <danitiana98@gmail.com>
2023-07-02 22:43:34 +0200

    Refactored preprocess

--- vid_cap/modelling/preprocessing/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Functions for preprocessing."""
-from ._feat_vec import ENCODER, IMG_PROCESSOR, gen_feat_vecs
-from .dataset import VideoFeatDataset
-
-__all__: list[str] = ["ENCODER", "gen_feat_vecs", "IMG_PROCESSOR", "VideoFeatDataset"]
--- vid_cap/scripts/prepare_dataset.py
+++ vid_cap/scripts/prepare_dataset.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Script to prepare dataset with VideoMAE."""
+from pathlib import Path
+
 import click
 import numpy as np
 import pandas as pd
@@ -10,15 +12,21 @@ from vid_cap.modelling import preprocessing as pre
 
 
 @click.command("prepare-dataset")
-def main() -> None:
-    """Prepare dataset with VideoMAE."""
+@click.option("--data-dir", default=DATA_DIR, type=click.Path(exists=True), help="Data directory")
+def main(data_dir: Path) -> None:
+    """Prepare dataset with ``VideoMAE``.
+
+    \f
+
+    :param data_dir: Path to data directory.
+    """
     logger.info("Preparing dataset")
 
     for split in ("test", "val", "train"):
         logger.info(f"Preparing {split} set")
 
-        captions_path = DATA_DIR / split / "captions.csv"
-        videos_path = DATA_DIR / split / "videos"
+        captions_path = data_dir / split / "captions.csv"
+        videos_path = data_dir / split / "videos"
 
         captions = pd.read_csv(captions_path, dtype_backend="pyarrow")[["video_id", "caption"]]
 
@@ -27,7 +35,7 @@ def main() -> None:
 
         out_caps = []
 
-        (DATA_DIR / "output" / split / "videos").mkdir(parents=True, exist_ok=True)
+        (data_dir / "output" / split / "videos").mkdir(parents=True, exist_ok=True)
 
         for video in videos_path.iterdir():
             logger.info(f"Processing video {counter+1}/{total} from {split} set")
@@ -37,7 +45,7 @@ def main() -> None:
 
             feat_vec = pre.gen_feat_vecs(video_path, 16)[0, :, :]
 
-            np.save(DATA_DIR / "output" / split / "videos" / f"{counter}.npy", feat_vec)
+            np.save(data_dir / "output" / split / "videos" / f"{counter}.npy", feat_vec)
 
             vid_caps = captions.loc[lambda x: x["video_id"] == stem, "caption"].to_list()
 
@@ -53,8 +61,8 @@ def main() -> None:
 
             counter += 1
 
-        pd.concat(out_caps).to_parquet(DATA_DIR / "output" / split / "captions.parquet")
+        pd.concat(out_caps).to_parquet(data_dir / "output" / split / "captions.parquet")
 
 
 if __name__ == "__main__":
-    main()
+    main.main(standalone_mode=False)
