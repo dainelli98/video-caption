9344b4bbb3d9c0a39bafb5493e1830087ddb863a
Daniel Martín <danitiana98@gmail.com>
2023-07-02 23:00:20 +0200

    Refactor dataset

--- vid_cap/dataset.py
+++ vid_cap/dataset.py
@@ -9,43 +9,62 @@ from torch.utils.data import Dataset
 
 
 class VideoFeatDataset(Dataset):
-    """Dataset with video feature vectors and associated captions."""
+    """Dataset with video feature vectors and associated captions.
+
+    :param video_dir: Directory with videos.
+    :param caps_file: Path to captions file.
+    :param caps_per_vid: Amount of captions per video, min=1, max=20.
+        Defaults to 1.
+    :param vocab_len: Amount of words for vocabulary. If ``None`` no vocab is built.
+    """
 
     _captions: pd.DataFrame
     _video_dir: Path
-    _vocab: list[str]
+    _vocab: dict[str, int] | None
 
     def __init__(
         self,
         video_dir: Path | str,
         caps_file: Path | str,
-        captions_amount_per_video: int = 1,
-        vocab_len: int = 10000,
+        caps_per_vid: int = 1,
+        vocab_len: int | None = None,
     ) -> None:
-        """Initialize dataset with data dictionary.
+        caps_per_vid = min(max(caps_per_vid, 1), 20)
 
-        :param data_dict: Dictionary with data.
-        :param captions_amount_per_video: Amount of captions per video, min=1, max=20.
-            Defaults to 1.
-        :param vocab_len: Amount of words for vocabulary. Defaults to 1000.
-        """
-        captions_amount_per_video = min(max(captions_amount_per_video, 1), 20)
-
-        self._captions = pd.read_parquet(caps_file)[
-            lambda x: x["n_cap"] <= captions_amount_per_video
-        ]
+        self._captions = pd.read_parquet(caps_file)[lambda x: x["n_cap"] <= caps_per_vid]
         self._video_dir = Path(video_dir)
 
         order = self._captions["caption"].str.len().sort_values().index
         self._captions = self._captions.reindex(order).reset_index(drop=True)
 
-        self._vocab = self._build_vocab(vocab_len)
+        if vocab_len is not None:
+            self._build_vocab(vocab_len)
 
-    def _build_vocab(self, vocab_len: int) -> dict[str, int]:
+    @property
+    def vocab(self) -> dict[str, int]:
+        """Get vocabulary.
+
+        :return: Vocabulary.
+        """
+        if self._vocab is None:
+            raise AttributeError("Vocabulary not built.")
+
+        return self._vocab
+
+    def get_vocab_len(self) -> int:
+        """Get vocabulary length.
+
+        :return: Vocabulary length.
+        """
+        if self._vocab is None:
+            raise AttributeError("Vocabulary not built.")
+
+        return len(self._vocab)
+
+    def build_vocab(self, vocab_len: int) -> None:
         """Build vocabulary from the captions in the dataset.
 
         :param vocab_len: Amount of words for vocabulary.
-        :return: Truncated sorted by frecuency vocab.
         """
         captions = self._captions["caption"].to_list()
         words = [word for caption in captions for word in caption.split()]
@@ -58,7 +77,7 @@ class VideoFeatDataset(Dataset):
         sorted_vocab.insert(0, "<pad>")
 
         truncated_sorted_vocab = sorted_vocab[:vocab_len]
-        return {token: idx for idx, token in enumerate(truncated_sorted_vocab)}
+        self._vocab = {token: idx for idx, token in enumerate(truncated_sorted_vocab)}
 
     def __getitem__(self, index: int) -> tuple[np.ndarray, str]:
         """Get item from dataset.
